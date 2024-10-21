98ac82fdf7808601f985689cc7770686ca948a92
ANUKGUERRA <anukguerra@gmail.com>
2024-10-21 12:43:48 +0200

    Finalitzada

---
+++ app/src/main/java/com/example/anuk_guerraizquierdo_uf1_act5/optionsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.anuk_guerraizquierdo_uf1_act5
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class optionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_options)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val toMainActivity = findViewById<Button>(R.id.toMain)
+        toMainActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
